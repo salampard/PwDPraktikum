d5e72072ec8b17e9278817c38d75622b9cb5ce5c
salampard <63052862+salampard@users.noreply.github.com>
2021-11-22 21:49:03 +0700

    Delete Praktikum4 directory

--- Praktikum4/hapus_user.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include "../koneksi.php";
-$sql = "delete from users where id_user='$_get[id]'";
-mysqli_query($con,$sql);
-mysqli_close($con);
-
-header('location:tampil_user.php');
--- Praktikum4/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-session_start();
-session_destroy();
-echo "Anda telah sukses keluar sistem <b> LogOut </b>";
