e29add86f4d90a688034524d207c576651d7d2c2
salampard <63052862+salampard@users.noreply.github.com>
2021-11-24 17:38:53 +0700

    Update form_login.php

--- Praktikum5/form_login.php
+++ Praktikum5/form_login.php
@@ -5,6 +5,11 @@ echo
 <table>
 <tr><td>Username</td><td>: <input type='text' name='id_user'></td></tr>
 <tr><td>Password</td><td>: <input type='password' name='paswd'></td></tr>
+<tr>
+<td>Captcha<br>
+<img src='../praktikum6/captcha.php' /></td>
+<td>:<input name='captcha_code' type='text'></td>
+</tr>
 <tr><td colspan=2></td><td><input type='submit' value='login'></td></tr>
 </table>
 </form>";
