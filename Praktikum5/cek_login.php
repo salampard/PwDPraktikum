d31007cfdd1bc9283428fb1d7c77a3e9cb63478e
salampard <63052862+salampard@users.noreply.github.com>
2021-11-24 17:39:32 +0700

    Update cek_login.php

--- Praktikum5/cek_login.php
+++ Praktikum5/cek_login.php
@@ -1,27 +1,33 @@
 <?php
+
+session_start();
 include "../koneksi.php";
 $id_user = $_POST['id_user'];
 $pass = md5($_POST['paswd']);
 $sql = "select * from users where id_user = '$id_user' and password = '$pass'";
 
-$login = mysqli_query($con, $sql);
+if ($_POST["captcha_code"] == $_SESSION["captcha_code"]) {
+    $login = mysqli_query($con, $sql);
 
-$ketemu = mysqli_num_rows($login);
-$r = mysqli_fetch_assoc($login);
+    $ketemu = mysqli_num_rows($login);
+    $r = mysqli_fetch_assoc($login);
 
-if ($ketemu > 0) {
-    session_start();
+    if ($ketemu > 0) {
 
-    $_SESSION['iduser'] = $r['id_user'];
-    $_SESSION['passuser'] = $r['password'];
+        $_SESSION['iduser'] = $r['id_user'];
+        $_SESSION['passuser'] = $r['password'];
 
-    echo "User Berhasil Login<br>";
-    echo "id user =", $_SESSION['iduser'], "<br>";
-    echo "password user =", $_SESSION['passuser'], "<br>";
-    echo "<center><a href=logout.php> <b>LOGOUT<b> </a></center>";
+        echo "User Berhasil Login<br>";
+        echo "id user =", $_SESSION['iduser'], "<br>";
+        echo "password user =", $_SESSION['passuser'], "<br>";
+        echo "<center><a href=logout.php> <b>LOGOUT<b> </a></center>";
+    } else {
+        echo "<center>Login gagal! username dan password tidak benar<br>";
+        echo "<a href=form_login.php><b>Ulangi Lagi</b></a></center>";
+    }
+
+    mysqli_close($con);
 } else {
-    echo "<center>Login gagal! username dan password tidak benar<br>";
+    echo "<center>Login gagal! Captcha tidak sesuai<br>";
     echo "<a href=form_login.php><b>Ulangi Lagi</b></a></center>";
 }
-
-mysqli_close($con);
